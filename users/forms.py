1f4f0d1a40cabf14557a928946d5e54b86fa94bf
Liebgard Becker <Liebgard.Becker@bssm.de>
2019-07-24 18:36:57 +0200

    8. Lektion komplett - Benutzerprofil anlegen

--- users/forms.py
+++ users/forms.py
@@ -8,5 +8,5 @@ class UserRegisterForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = ["username", "email", "password1", "password2"]
 
--- users/views.py
+++ users/views.py
@@ -3,18 +3,23 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm
 
+
 def register(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
-            username = form.cleaned_data.get('username')
-            messages.success(request, f'Sie sind jetzt Mitgleid bei Wochenmarkt. Sie können Sich jetzt einloggen')
-            return redirect('login')
+            username = form.cleaned_data.get("username")
+            messages.success(
+                request,
+                f"Sie sind jetzt Mitglied bei Wochenmarkt. Sie können Sich jetzt einloggen",
+            )
+            return redirect("login")
     else:
         form = UserRegisterForm()
-    return render(request, 'users/register.html', {'form': form})
+    return render(request, "users/register.html", {"form": form})
+
 
 @login_required
 def profile(request):
-    return render(request, 'users/profile.html')
+    return render(request, "users/profile.html")
